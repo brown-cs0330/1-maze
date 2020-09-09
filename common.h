c4ed2cca6aa5b95340d02e08cb38d12899bd47c1
Ian Layzer <ianlayzer@gmail.com>
2020-09-09 17:04:27 -0400

    Updated Stencil code and added maze validator executable

--- common.h
+++ common.h
@@ -12,20 +12,25 @@ typedef enum { NORTH = 0, SOUTH = 1, WEST = 2, EAST = 3 } Direction;
 /*
  * Struct to represent a room in the maze.
  * Must store:
- * 	- The location of the room
- * 	- Whether or not this room has been visited
+ *  - The location of the room
+ *  - Whether or not this room has been visited
  *      - A pointer to the next room (provided), set this pointer
  *        in the current room's struct to the next room you visit
- *	- The connections to neighboring rooms in all
- *	  four directions.
+ *  - The connections to neighboring rooms in all
+ *    four directions.
  */
 struct maze_room {
+    // Fill in the fields you need
+    
+    // NOTE: ignore this field until you start implementing the solver
     struct maze_room *next;
-    // Fill in the rest of the fields you need
 };
 
 int is_in_range(int row, int col, int num_rows, int num_cols);
 
 struct maze_room *get_neighbor(int num_rows, int num_cols,
-                               struct maze_room maze[][num_cols],
+                               struct maze_room maze[num_rows][num_cols],
                                struct maze_room *room, Direction dir);
+
+void initialize_maze(int num_rows, int num_cols,
+           struct maze_room maze[num_rows][num_cols]);
--- generator.c
+++ generator.c
@@ -9,26 +9,26 @@
  * Given a direction, get its opposite
  *
  * Parameters:
- *	- dir: direction
+ *  - dir: direction
  *
  * Returns:
- *	- the opposite direction to dir
+ *  - the opposite direction to dir
  */
 Direction get_opposite_dir(Direction dir) {
-    // Fill in this function
+    // TODO: implement this function
 }
 
 /*
  * Given an array of the four directions, randomizes the order of the directions
  *
  * Parameters:
- *	- directions: an array of Direction enums of size 4
+ *  - directions: an array of Direction enums of size 4
  *
  * Returns:
- *	- nothing - the array should be shuffled in place
+ *  - nothing - the array should be shuffled in place
  */
 void shuffle_array(Direction directions[]) {
-    // Fill in this function
+    // TODO: implement this function
 }
 
 /*
@@ -36,66 +36,116 @@ void shuffle_array(Direction directions[]) {
  *connections/walls using the drunken_walk algorithm
  *
  * Parameters:
- * 	- row: row of the current room
- *	- col: column of the current room
- *	- num_rows: number of rows in the maze
- *	- num_cols: number of columns in the maze
- *	- maze: a 2D array of maze_room structs representing your maze
+ *  - row: row of the current room
+ *  - col: column of the current room
+ *  - num_rows: number of rows in the maze
+ *  - num_cols: number of columns in the maze
+ *  - maze: a 2D array of maze_room structs representing your maze
  * Returns:
- *	- nothing - the state of your maze should be saved in the maze array
+ *  - nothing - the state of your maze should be saved in the maze array
  *being passed in (make sure to use pointers correctly!).
  */
 void drunken_walk(int row, int col, int num_rows, int num_cols,
                   struct maze_room maze[num_rows][num_cols]) {
-    // Fill in this function
+    // TODO: implement this function
 }
 
 /*
- * Represents a maze_room struct as a hexadecimal number based on its
- *connections
+ * Represents a maze_room struct as an integer based on its connections
  *
  * Parameters:
- *	- room: a struct maze_room to be converted to a hexadecimal number
+ *  - room: a struct maze_room to be converted to an integer
  *
  * Returns:
- *	- the hexadecimal representation of room
+ *  - the integer representation of a room
  */
 int encode_room(struct maze_room room) {
-    // Fill in this function
+    // TODO: implement this function
 }
 
 /*
  * Represents a maze_room array as a hexadecimal array based on its connections
  *
  * Parameters:
- *	- num_rows: number of rows in the maze
- *	- num_cols: number of columns in the maze
- *	- maze: a 2D array of maze_room structs representing the maze
- *	- result: a 2D array of hexadecimal numbers representing the maze
+ *  - num_rows: number of rows in the maze
+ *  - num_cols: number of columns in the maze
+ *  - maze: a 2D array of maze_room structs representing the maze
+ *  - result: a 2D array of hexadecimal numbers representing the maze
  *
  * Returns:
- *	- nothing - each maze_room in the maze should be represented
- *	  as a hexadecimal number  and put in nums at the corresponding index
+ *  - nothing - each maze_room in the maze should be represented
+ *    as a hexadecimal number  and put in nums at the corresponding index
  */
 void encode_maze(int num_rows, int num_cols,
                  struct maze_room maze[num_rows][num_cols],
                  int result[num_rows][num_cols]) {
-    // Fill in this function
+    // TODO: implement this function
+}
+
+/*
+ * Writes encoded maze to an output file
+ *
+ * Parameters:
+ *  - num_rows: number of rows in the maze
+ *  - num_cols: number of columns in the maze
+ *  - encoded_maze: a maze_room array as a hexadecimal array based on its
+ *    connections
+ *  - file_name: the name of the output file for the encoded maze
+ *
+ * Returns:
+ *  - 1 if an error occurs, 0 otherwise
+ */
+int write_encoded_maze_to_file(int num_rows, int num_cols,
+                               int encoded_maze[num_rows][num_cols],
+                               char *file_name) {
+    int err = 0;
+
+    // open file (create it if necessary)
+    FILE *f = fopen(file_name, "w+");
+    if (f == NULL) {
+        fprintf(stderr, "Error opening file.\n");
+        return 1;
+    }
+    // write each room's hex value into file
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            int encoded_room = encoded_maze[i][j];
+            err = fprintf(f, "%x", encoded_room);
+            if (err < 0) {
+                fprintf(stderr, "Writing to file failed: %i\n", err);
+                return 1;
+            }
+        }
+        // add a newline between rows
+        err = fprintf(f, "\n");
+        if (err < 0) {
+            fprintf(stderr, "Writing to file failed: %i\n", err);
+            return 1;
+        }
+    }
+    // close file
+    int close = fclose(f);
+    if (close == EOF) {
+        fprintf(stderr, "Could not close file.\n");
+        return 1;
+    }
+    return 0;
 }
 
 /*
  * Main function
  *
  * Parameters:
- *	- argc: the number of command line arguments - for this function 4
- *	- **argv: a pointer to the first element in the command line
+ *  - argc: the number of command line arguments - for this function 4
+ *  - **argv: a pointer to the first element in the command line
  *            arguments array - for this function:
- *		      ["generator", <output file>, <number of rows>, <number of
+ *            ["generator", <output file>, <number of rows>, <number of
  *columns>]
  *
  * Returns:
- *	- 0 if program exits correctly, 1 if there is an error
+ *  - 0 if program exits correctly, 1 if there is an error
  */
+
 int main(int argc, char **argv) {
     char *file_name;
     int num_rows;
@@ -111,6 +161,5 @@ int main(int argc, char **argv) {
         num_rows = atoi(argv[2]);
         num_cols = atoi(argv[3]);
     }
-
-    // Fill in this function
+    // TODO: implement this function
 }
